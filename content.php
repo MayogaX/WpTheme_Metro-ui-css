0db2aec387693ef2ff30d7889f4d376e4e9290c9
MayogaX <mayumisatox@gmail.com>
2013-04-01 06:41:46 -0300

    alter the links for the articles

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_MetroUiCss' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_MetroUiCss' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><h2 class="entry-title"><?php the_title(); ?></h2></a>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
